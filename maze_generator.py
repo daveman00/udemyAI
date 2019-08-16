e7ebae4a7e494d5e4bc25e19f16545127001d27d
daveman00 <czyrny.dawid@gmail.com>
2019-08-16 12:38:14 +0200

    maze_generator.py comment cleanup

--- maze_generator.py
+++ maze_generator.py
@@ -3,25 +3,32 @@
 import numpy
 
 
+# Maze class - handles generation of square maze using Hunt'n'Kill algorithm
 class Maze:
-#    size = 0
-#    grid = []
-#    visit_map = []
+    # dictionary with direction vectors
     directions = {'N': (-2,0),
                   'W': (0,-2),
                   'S':  (2,0),
                   'E':  (0,2),
                   }
 
+    # initialize Maze class - size should be an odd number
     def __init__(self, size=9):
         if size % 2 == 0:
             self.size = size + 1
         else:
             self.size = size
+        # initialize grid
         self.grid = self.generate_grid()
+        # copy grid to visit_map to track the changes in maze generation
         self.visit_map = numpy.copy(self.grid)
-        #self.visit_map = self.grid.copy()
 
+    # fill maze grid with empty cells in the following manner:
+    #   1 1 1 1 1
+    #   1 0 1 0 1
+    #   1 1 1 1 1
+    #   1 0 1 0 1
+    #   1 1 1 1 1
     def generate_grid(self):
         grid = numpy.ones(shape=(self.size, self.size))
         for i in range(1,grid.shape[0], 2):
@@ -29,18 +36,17 @@ class Maze:
                 grid[i,j] = 0
         return numpy.array(grid)
 
+    # return coordinates of random unvisited cell
     def get_starting_position(self):
         zeroes = numpy.argwhere(self.grid == 0)
         return tuple(zeroes[numpy.random.choice(len(zeroes))])
 
-    # visited - can be 0 or 1 - determines if looking for unvisited(0) or visited cells(1)
+    # check available directions from given point
+    # state - can be 0 or 1 - determines if looking for unvisited(0) or visited cells(1) in the neighbourhood
     def check_directions(self, point, state=0):
         available_directions = []
         for dir in self.directions:
             neighbour = numpy.add(point,self.directions[dir])
-#            print('NEIGHBOUR: ', neighbour)
-            #print("VISIT MAP IN CHECK_DIRECTIONS:")
-            #color_print_map(self.visit_map)
             try:
                 if any(n < 0 for n in neighbour):
                     raise IndexError('Negative index: ', neighbour)
@@ -52,98 +58,49 @@ class Maze:
             else:
                 available_directions.append(neighbour)
         return available_directions
-#            self.carve_passage(point, neighbour)
-#        available_directions = []
-#        for direction in self.directions:
-#            new_dir = tuple(numpy.add(self.directions[direction], list(point)))
-#            try:
-#                if self.visit_map[new_dir] == 0:
-#                    available_directions.append(new_dir)
-#                else:
-#                    raise ValueError('Visited cell', new_dir)
-#            except (IndexError,ValueError) as e:
-#                print(e)
-#                continue
-#        return available_directions
 
+    # carve passage from unvisited to previously visited cell
     def carve_passage(self, start, end):
-#        print("DIR: ", end)
         passage = tuple((numpy.add(start, end)/2).astype(int))
-#        print("PASS: ", passage)
         self.visit_map[end] = 1
-        #self.visit_map[start] = 1
         self.grid[passage] = 0
 
+    # function carves continuous passages from start point until dead end is reached
     def kill(self, start):
-#        start = self.get_starting_position()i
-#        print("START TYPE: ", type(start))
         start = tuple(start)
-#        print("START: ", start)
-#        tmp = numpy.copy(self.visit_map)
-#        print("VISIT MAP BEFORE:")
-#        color_print_map(self.visit_map)
         self.visit_map[start] = 1
-#        print("VISIT MAP DIFF:")
-#        color_print_map(numpy.subtract(self.visit_map,tmp))
-#        print("VISIT MAP AFTER:")
-#        color_print_map(self.visit_map)
-#        self.grid[start] = 2
         while True:
             directions = self.check_directions(start)
-#            print("DIRECTIONS: ", directions)
             if not directions: break
             direction = tuple(directions[numpy.random.choice(len(directions))])
             self.carve_passage(start, direction)
-#            print("DIR: ", direction)
-#            passage = tuple((numpy.add(start, direction)/2).astype(int))
-#            print("PASS: ", passage)
-#            self.visit_map[direction] = 1
-#            self.grid[passage] = 0
             start = direction
-#            print('NEW START: ', start)
 
+    # function searches for unvisited cell with visited neighbours to continue carving of the maze in new direction
+    # returns unvisited cell coordinates as new starting point
     def hunt(self):
         unvisited = numpy.argwhere(self.visit_map == 0)
-#        print('UNVISITED: ', unvisited)
         for point in unvisited:
-#            print('POINT: ', point)
             available_directions = self.check_directions(point,1)
-#            print('AVAILABLE DIRECTIONS: ', available_directions)
             if available_directions:
                 direction = tuple(available_directions[numpy.random.choice(len(available_directions))])
-#                print('CARVING DIRECTION: ', direction)
                 self.carve_passage(point, direction)
-#                print('NEW START POINT: ', point)
-                #print('NEW START POINT: ', direction)
                 return point
-                #return direction
         return None
 
+    # main loop - kill function carves continuous passages,
+    # when reaching dead end hunt function is triggered allowing further carving
+    # until all cells have been visited
     def hunt_n_kill(self):
         start = self.get_starting_position()
         while True:
-#            print('start: ', start)
-#            print('KILLING')
             self.kill(start)
-#            print('GRID:')
-#            color_print_map(self.grid)
-#            print('VISIT MAP:')
-#            color_print_map(self.visit_map)
-#            input("continue...1")
-#            print("==========")
-#            print("HUNTING")
             start = self.hunt()
-#            print('GRID:')
-#            color_print_map(self.grid)
-#            print('VISIT MAP:')
-#            color_print_map(self.visit_map)
-#            print("==========")
-#            input("continue...2")
             if start is None: break
 
 
 # debugging purposes
-def color_print_map(arr, color='OKBLUE'):
+def color_print_map(arr):
     BLUE = '\x1b[1;34m'
     RED = '\x1b[1;31m'
     ENDC = '\x1b[0m'
@@ -156,15 +113,10 @@ def color_print_map(arr, color='OKBLUE'):
 
         print()
 
-maze = Maze(11)
 
-#color_print_map(maze.grid)
-print()
-maze.hunt_n_kill()
-#maze.kill(maze.get_starting_position())
-print('==============')
-color_print_map(maze.grid)
-print('==============')
-#color_print_map(maze.visit_map)
-
-#print(maze.hunt())
+if __name__ == '__main__':
+    maze = Maze(11)
+    maze.hunt_n_kill()
+    print('==============')
+    color_print_map(maze.grid)
+    print('==============')
